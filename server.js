a4912e180a91d135d56dc247e98a347722008fa7
Pranav Pratap Singh <pranavpratapsingh108@gmail.com>
2022-11-22 10:21:49 +0530

    added body-parser

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const routes = require("./routes/course.routes");
 
 // create our express app
 const app = express();
 // middleware
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 // route
